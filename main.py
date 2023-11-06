d2c67181903bc4001a15d9b7e7fa4e12541696d5
jethro <jethro.cotton3@gmail.com>
2023-11-06 15:15:13 +0200

    got most of the forms implemented

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+##TODO Link newEntry and newModule
+##Refactor a bunch of the GUI refresh stuff
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox, QInputDialog
 import sys
@@ -6,7 +9,7 @@ import newModule
 import mainUI
 import sql
 import newEntry
-import newModule
+import moduleView
 
 sql_conn = sql.Connection()
 sql_conn.create_module_table()
@@ -32,10 +35,29 @@ def create_newmodule_window(MainWindow):
     newMod.new_mod_confirm.clicked.connect(lambda: MainWindow.second_window.close())
 
 
-def select_module():
+def select_module(mainUI):
+    current_mod = sql_conn.get_module_by_name(ui.module_list.currentItem().text())
     current_mod_entries = sql_conn.get_module_entries_by_code(ui.module_list.currentItem().text())
-    ui.module_list.clear()
-    ui.module_list.addItems([str(module).strip("',)(") for module in current_mod_entries])
+    Form = QtWidgets.QWidget()
+    selectedMod = moduleView.Ui_module_view()
+    selectedMod.setupUi(Form)
+    #selectedMod.label.setText(current_mod)
+    MainWindow.second_window = Form
+    Form.setWindowTitle("Module View")
+    Form.setFocus()
+    #Form.new_mod_cancel.clicked.connect(lambda: MainWindow.second_window.close())
+    MainWindow.second_window.show()
+
+def create_new_position(mainUI):
+    Form = QtWidgets.QWidget()
+    newPositionEntry = newEntry.Ui_Form()
+    newPositionEntry.setupUi(Form)
+    MainWindow.third_window = Form
+    Form.setWindowTitle("New Entry")
+    Form.setFocus()
+    #newEntry.new_entry_cancel.clicked.connect(lambda: MainWindow.third_window.close())
+    MainWindow.third_window.show()
+
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
@@ -44,7 +66,8 @@ ui.setupUi(MainWindow)
 MainWindow.show()
 MainWindow.setWindowTitle("Module Position Manager")
 ui.menuNew.triggered.connect(lambda: create_newmodule_window(mainUI.Ui_MainWindow))
-ui.select_btn.clicked.connect(lambda: select_module())
+ui.create_position_btn.clicked.connect(lambda: create_new_position(mainUI.Ui_MainWindow))
+ui.select_btn.clicked.connect(lambda: select_module(mainUI.Ui_MainWindow))
 ui.module_list.addItems([str(module).strip("',)(") for module in all_modules])
 sys.exit(app.exec_())
 
